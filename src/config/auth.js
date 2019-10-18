e0c08e21371d3c9b6a5e2a29cbc8d29be4022199
Patrick Maciel <patrickmaciel.info@gmail.com>
2019-10-18 00:59:52 -0300

    Feature - session login and user store-update

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b9e9e82d737efadb29af7617fca6abd6',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Jesus é rei' });
-});
+routes.post('/login', SessionController.login);
+
+routes.use(authMiddleware);
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
